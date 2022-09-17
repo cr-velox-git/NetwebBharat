b6c55a3aa5c10c2dc4bb6354e8b7fd06e810fa3a
Debendra@5522 <debendra.b@iserveu.co.in>
2022-09-17 17:41:03 +0530

    MATM add

---
+++ app/src/main/java/com/netweb/netwebbharat/MATM/TransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.netweb.netwebbharat.MATM
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.netweb.netwebbharat.R
+
+class TransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction)
+    }
+}
